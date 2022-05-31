081c603e4a02aacb84b652bc7fcdf7b852b1fb2f
juleaume <guillaume.j.gautier@gmail.com>
2022-05-31 22:50:46 +0200

    Removed useless kv language and use python only to make screen changer in tests

--- tests/_tests.py
+++ tests/_tests.py
@@ -1,48 +1,93 @@
+import random
+
 from kivy.app import App
-from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.uix.screenmanager import ScreenManager, Screen
 
-# Create both screens. Please note the root.manager.current: this is how
-# you can control the ScreenManager from kv. Each screen has by default a
-# property manager that gives you the instance of the ScreenManager used.
-Builder.load_string("""
-<MenuScreen>:
-    BoxLayout:
-        Button:
-            text: 'Goto settings'
-            on_press: root.manager.current = 'settings'
-        Button:
-            text: 'Quit'
-            on_press: app.stop()
 
-<SettingsScreen>:
-    BoxLayout:
-        Button:
-            text: 'My settings button'
-        Button:
-            text: 'Back to menu'
-            on_press: root.manager.current = 'menu'
-""")
+def name_factory(order):
+    def name():
+        return order
+
+    return name()
+
+
+class MyScreenManager(ScreenManager):
+    def __init__(self, **kwargs):
+        super(MyScreenManager, self).__init__(**kwargs)
+        self._setup_screens()
+
+    def _setup_screens(self):
+        def make_screen(_screen: Screen):
+            layout = BoxLayout(orientation="vertical")
+            layout.add_widget(Label(text=screen.name))
+            layout.add_widget(
+                Button(
+                    text="Back to settings",
+                    on_press=self.switch_to_settings
+                )
+            )
+            _screen.add_widget(layout)
+        self.screens = [Screen(name=name_factory(f"Screen {i}")) for i in
+                        range(4)]
+        print(self.screens)
+        for screen in self.screens:
+            make_screen(screen)
+        self.menu_screen = MenuScreen(name="menu")
+        self.setting_screen = SettingsScreen(name='settings')
+        self.menu_screen.exit_button.on_press = exit
+        self.menu_screen.setting_button.on_press = self.switch_to_settings
+        self.setting_screen.menu_button.on_press = self.switch_to_menu
+        self.setting_screen.setting_button.on_press = self.switch_to_rnd
+        self.add_widget(self.menu_screen)
+        self.add_widget(self.setting_screen)
+
+    def switch_to_settings(self, *args):
+        self.transition.direction = "left" if len(args) == 0 \
+            else "down"
+        self.switch_to(self.setting_screen)
+
+    def switch_to_menu(self):
+        self.transition.direction = "right"
+        self.switch_to(self.menu_screen)
+
+    def switch_to_rnd(self):
+        self.transition.direction = "up"
+        self.switch_to(random.choice(self.screens))
 
 
 # Declare both screens
 class MenuScreen(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.layout = BoxLayout()
+        self.add_widget(self.layout)
+        self.setting_button = Button(text="Settings")
+        self.layout.add_widget(self.setting_button)
+
+        self.exit_button = Button(text="Quit")
+        self.layout.add_widget(self.exit_button)
 
 
 class SettingsScreen(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.layout = BoxLayout()
+        self.add_widget(self.layout)
+        self.setting_button = Button(text="Go to...")
+        self.layout.add_widget(self.setting_button)
+
+        self.menu_button = Button(text="Back to menu")
+        self.layout.add_widget(self.menu_button)
 
 
 class TestApp(App):
 
     def build(self):
         # Create the screen manager
-        sm = ScreenManager()
-        sm.add_widget(MenuScreen(name='menu'))
-        sm.add_widget(SettingsScreen(name='settings'))
-
-        return sm
+        return MyScreenManager()
 
 
 if __name__ == '__main__':
