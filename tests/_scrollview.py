003520e06b033ac75d8d1108456405a64a1bc088
juleaume <guillaume.j.gautier@gmail.com>
2022-06-04 19:41:53 +0200

    tested 3 col

--- tests/_scrollview.py
+++ tests/_scrollview.py
@@ -4,7 +4,7 @@ from kivy.uix.scrollview import ScrollView
 from kivy.core.window import Window
 from kivy.app import runTouchApp
 
-layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
+layout = GridLayout(cols=3, spacing=10, size_hint_y=None)
 # Make sure the height is such that there is something to scroll.
 layout.bind(minimum_height=layout.setter('height'))
 for i in range(100):
